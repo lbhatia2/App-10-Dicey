19e1e47a0f3b2f5044e0fc25d4b00003618e2a5d
Lina Bhatia <lina@bhatia.org>
2022-07-29 11:33:01 -0500

    Added camera effect

--- App 10 Dicey/ContentView.swift
+++ App 10 Dicey/ContentView.swift
@@ -9,13 +9,19 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         VStack {
             Image("pips \(randomValue)")
                 .resizable()
                 .frame(width: 200, height: 200, alignment: .center)
+                .rotationEffect(.degrees(rotation))
+                .rotation3DEffect(.degrees(rotation), axis:(x: 1, y:1, z: 0))
                 .onTapGesture {
                     randomValue = Int.random(in: 1...6)
+                    withAnimation(.default) {
+                        rotation += 360
+                    }
                 }
             Spacer()
             
