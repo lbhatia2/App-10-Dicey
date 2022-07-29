baeedc2d91cfc4bc63cd932d033a496f94f3a80e
Lina Bhatia <lina@bhatia.org>
2022-07-29 11:15:38 -0500

    Initial Commit

---
+++ App 10 Dicey/App_10_DiceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_10_DiceyApp.swift
+//  App 10 Dicey
+//
+//  Created by Lina Bhatia on 7/29/22.
+//
+
+import SwiftUI
+
+@main
+struct App_10_DiceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ App 10 Dicey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  App 10 Dicey
+//
+//  Created by Lina Bhatia on 7/29/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
